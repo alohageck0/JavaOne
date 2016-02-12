400b62083bc7083ff23f7d9cbd8c54270a19b8d1
alohageck0 <royalfiish@gmail.com>
2016-02-11 23:08:02 -0800

    udemy practice 02/11/16

--- ByteShortInt/src/Challenge.java
+++ ByteShortInt/src/Challenge.java
@@ -1,9 +1,11 @@
 public class Challenge {
     public static void main(String[] args) {
         byte myByte = 44;
-        short myShort = 6534;
-        int myInt = 17_373_343;
+        short myShort = 65;
+        int myInt = 17;
         long myLong = 50000L + 10L * (myByte + myShort + myInt);
+        short shortTotal = (short) (1000 + 10 * (myByte + myShort + myInt));
         System.out.println(myLong);
+        System.out.println(shortTotal);
     }
 }
