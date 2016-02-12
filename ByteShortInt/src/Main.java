efcaa440233a6caa5d46e8512ed219bb89f18567
alohageck0 <royalfiish@gmail.com>
2016-02-11 21:50:38 -0800

    udemy practice 02/11/16

---
+++ ByteShortInt/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+
+        byte myByteValue = 127;
+
+    }
+}
+
