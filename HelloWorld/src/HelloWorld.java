40b9e5870fa09d2375396fedbafa28c3d4a64e00
alohageck0 <royalfiish@gmail.com>
2016-02-11 21:26:29 -0800

    udemy practice 02/11/16

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFirstNumber = 5;
+        int second = 6;
+        int third = 222;
+
+        int total = myFirstNumber + second + third;
+
+        int newTotal = 1000 - total;
+        System.out.println(newTotal);
     }
 }
