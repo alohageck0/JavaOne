9e05e992ba20b6ae2067d7373f114d791865920f
alohageck0 <royalfiish@gmail.com>
2016-02-13 18:38:19 -0800

    udemy practice 02/13/16

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //width of 16 - 2 bytes
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBool = false;
+        boolean isMale = true;
+
+        char registred = '\u00AE';
+        System.out.println(registred);
+
+    }
+}
