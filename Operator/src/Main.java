ec0fa7916cb7202992f71d695028786adc5dc690
alohageck0 <royalfiish@gmail.com>
2016-02-13 20:55:06 -0800

    udemy practice 02/13/16

--- Operator/src/Main.java
+++ Operator/src/Main.java
@@ -45,8 +45,35 @@ public class Main {
             System.out.println("high score!");
         }
         int secondTopScore = 60;
-        if (topScore>secondTopScore && topScore<100)
-            System.out.println("Greater than second top sore and less then 100");
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second " +
+                    "top sore and less then 100");
+        }
+        if ((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of this test is true");
+
+        int newVlue = 50;
+        if (newVlue == 50)
+            System.out.println("this is true");
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not suppose to ahppen");
+        }
+
+//        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+            System.out.println("wasCar is true");
+
+        double valOne = 20d;
+        double valTwo = 80d;
+        double result = (valOne + valTwo) * 25;
+        System.out.println("My total = " + result);
+        double reminder = result % 40;
+        System.out.println(reminder);
+        if (reminder <= 20)
+            System.out.println("Total was over the limit");
 
     }
 }
