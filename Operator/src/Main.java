29515b38af6d204e445199a1d09aab6e795a81cf
alohageck0 <royalfiish@gmail.com>
2016-02-13 20:27:05 -0800

    udemy practice 02/13/16

---
+++ Operator/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+//        int result = 1 + 2;
+//        System.out.println(result);
+//
+//        int prevResult = result;
+//        result = result - 1;
+//
+//        System.out.println(prevResult + " " + result);
+//        prevResult = result;
+//        result = result * 10;
+//        System.out.println(prevResult + " *10 = " + result);
+//
+//        prevResult = result;
+//        result = result / 5;
+//        System.out.println(prevResult + " /5 = " + result);
+//
+//        prevResult = result;
+//        result = result % 3;
+//        System.out.println("reminder " + prevResult + " /3 " + result);
+//        result++;
+//        System.out.println(result);
+//
+//        result--;
+//        System.out.println(result);
+//
+//        result += 2;
+//        System.out.println(result);
+//
+//        result *= 10;
+//        System.out.println(result);
+//
+//        result -= 10;
+//        System.out.println(result);
+//
+//        result /= 10;
+//        System.out.println(result);
+
+        boolean isAlien = false;
+        if (isAlien == true) {
+            System.out.println("It is not an alien");
+        }
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("high score!");
+        }
+        int secondTopScore = 60;
+        if (topScore>secondTopScore && topScore<100)
+            System.out.println("Greater than second top sore and less then 100");
+
+    }
+}
