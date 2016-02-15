a809e0c9844a9464e16229af2907e3a5cd2fe7f4
alohageck0 <royalfiish@gmail.com>
2016-02-14 18:01:36 -0800

    udemy practice 02/14/16

---
+++ Statements/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+   public static void main(String[] args) {
+
+      int myVar = 50;
+      if (myVar == 50) {
+         System.out.println("true");
+      }
+      myVar++;
+      myVar--;
+
+      System.out.println("test" +
+              " test2" +
+              " test3");
+
+      int anotherVar = 50;
+      myVar--;
+      System.out.println("sdfsdfds");
+
+
+   }
+}
