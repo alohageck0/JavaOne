6f88ce26db14062fe82d77c88fda0f708ed839f5
alohageck0 <royalfiish@gmail.com>
2016-02-14 19:03:53 -0800

    udemy practice 02/14/16

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
    public static void main(String[] args) {
-      calculateScore();
-
-   }
-
-   public static void calculateScore() {
       boolean gameOver = true;
       int score = 4000;
       int levelCompleted = 5;
       int bonus = 100;
 
+      calculateScore(gameOver, score, levelCompleted, bonus);
+
+      calculateScore(true, 10000, 8, 200);
+
+
+   }
+
+   public static void calculateScore(boolean gameOver, int score,
+                                     int levelCompleted, int bonus) {
       if (gameOver) {
          int finalScore = score + (levelCompleted * bonus);
+         finalScore+=1000;
          System.out.println(finalScore);
       }
    }
