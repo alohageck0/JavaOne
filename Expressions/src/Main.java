bda0d4ff66f212b68a783693ae2ca03dbfebc256
alohageck0 <royalfiish@gmail.com>
2016-02-14 17:48:38 -0800

    udemy practice 02/14/16

---
+++ Expressions/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int int2 = 5;
+        //false, true, null
+        // a mile is 1.609344
+
+        double kiloms = (100 * 1.609344);
+        int highscore = 50;
+        if (highscore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // score = 90, score<90, text, score = 0
+
+
+    }
+}
