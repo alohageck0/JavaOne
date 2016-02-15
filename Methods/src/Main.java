d2fb4e3a1689aa89c3ac97623324757ef01a2ee5
alohageck0 <royalfiish@gmail.com>
2016-02-14 20:46:17 -0800

    udemy practice 02/14/16

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -5,10 +5,18 @@ public class Main {
       int levelCompleted = 5;
       int bonus = 100;
 
-      calculateScore(gameOver, score, levelCompleted, bonus);
+      int highscore = calculateScore(gameOver, score, levelCompleted, bonus);
+      System.out.println("Your final score " + highscore);
 
-      calculateScore(true, 10000, 8, 200);
 
+      highscore = calculateScore(true, 10000, 8, 200);
+      System.out.println("Your final score " + highscore);
+
+      displayHighScorePosition("Test", 1500);
+      displayHighScorePosition("Test", 900);
+      displayHighScorePosition("Test", 400);
+      displayHighScorePosition("Test", 50);
+      displayHighScorePosition("Test", 1000);
 
    }
 
@@ -20,6 +28,23 @@ public class Main {
          return finalScore;
       }
       return -1;
+   }
+
+   public static void displayHighScorePosition(String name, int highscore) {
+      System.out.println(name + " managed to get into position " +
+              calculateHighScorePosition(highscore) + " on the high score table");
+   }
+
+   public static int calculateHighScorePosition(int score) {
+      if (score >= 1000) {
+         return 1;
+      } else if (score >= 500 && score < 1000) {
+         return 2;
+      } else if (score >= 100 && score < 500) {
+         return 3;
+      } else {
+         return 4;
+      }
    }
 }
 
