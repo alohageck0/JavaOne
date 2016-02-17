633374d1c1e8459a0e98edb984fbb660bf8e8ba9
alohageck0 <royalfiish@gmail.com>
2016-02-16 22:22:13 -0800

    home udemy practice 02/16/16

--- Constructors/src/Account.java
+++ Constructors/src/Account.java
@@ -5,6 +5,21 @@ public class Account {
    private String email;
    private String phone;
 
+
+   public Account() {
+      this(24234, 2.5, "default", "def email", "def phone");
+      System.out.println("Empty constructor");
+   }
+
+   public Account(int number, double balance, String customerName, String email, String phone) {
+      this.accNumber = number;
+      this.balance = balance;
+      this.customerName = customerName;
+      this.email = email;
+      this.phone = phone;
+      System.out.println("With params");
+   }
+
    public int getAccNumber() {
       return accNumber;
    }
@@ -46,14 +61,14 @@ public class Account {
       this.phone = phone;
    }
 
-   public void deposit(double ammount) {
-      this.balance += ammount;
+   public void deposit(double amount) {
+      this.balance += amount;
       System.out.println("New balance = " + this.balance);
    }
 
-   public void withdraw(double ammount) {
-      if ((this.balance - ammount) >= 0) {
-         this.balance -= ammount;
+   public void withdraw(double amount) {
+      if ((this.balance - amount) >= 0) {
+         this.balance -= amount;
          System.out.println("New balance = " + this.balance);
       } else {
          System.out.println("Not enough money");
--- Constructors/src/Main.java
+++ Constructors/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
    public static void main(String[] args) {
       //start
-      Account test = new Account();
+      Account test = new Account();//123123, 0.00, "Bob", "444@email", "234234");
+      System.out.println(test.getBalance());
+
       test.setBalance(1000);
       test.deposit(500);
       test.withdraw(2000);
+
    }
 }
