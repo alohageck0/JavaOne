323d8b941a4edc018e3bfad281e7648f9ee4cfff
alohageck0 <royalfiish@gmail.com>
2016-02-23 07:32:27 -0800

    home udemy practice 02/23/16

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,18 +1,38 @@
-public class Main {
-   public static void main(String[] args) {
-      int[] myIntArray = new int[25];// = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//      myIntArray[0] = 45;
-//      myIntArray[5] = 50;
+import java.util.Scanner;
 
-      for (int i = 0; i < myIntArray.length; i++) {
-         myIntArray[i] = i * 10;
+public class Main {
+   private static Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) {
+//      int[] myIntArray = new int[25];// = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+////      myIntArray[0] = 45;
+////      myIntArray[5] = 50;
+//
+//      for (int i = 0; i < myIntArray.length; i++) {
+//         myIntArray[i] = i * 10;
+//      }
+//      for (int i = 0; i < myIntArray.length; i++) {
+//         System.out.println(myIntArray[i]);
+//      }
+////      System.out.println(myIntArray[5]);
+////      System.out.println(myIntArray[2]);
+//      printArray(myIntArray);
+
+      int[] myInt = getIntegers(5);
+      for (int i = 0; i < myInt.length; i++) {
+         System.out.println("Element " + i + " = " + myInt[i]);
       }
-      for (int i = 0; i < myIntArray.length; i++) {
-         System.out.println(myIntArray[i]);
+      System.out.println("the Avg is " + getAvg(myInt));
+
+   }
+
+   private static int[] getIntegers(int number) {
+      System.out.println("Enter " + number + "integer values. \r");
+      int[] values = new int[number];
+      for (int i = 0; i < values.length; i++) {
+         values[i] = scanner.nextInt();
       }
-//      System.out.println(myIntArray[5]);
-//      System.out.println(myIntArray[2]);
-      printArray(myIntArray);
+      return values;
    }
 
    public static void printArray(int[] array) {
@@ -20,5 +40,13 @@ public class Main {
          System.out.println(array[i]);
       }
 
+   }
+
+   public static double getAvg(int[] array) {
+      int sum = 0;
+      for (int i = 0; i < array.length; i++) {
+         sum += array[i];
+      }
+      return (double) sum / array.length;
    }
 }
