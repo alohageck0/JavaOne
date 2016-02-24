ff4dfbc59fe661fbfd4cc45c9acd8f1153dcd3af
Evgenii <royalfiish@gmail.com>
2016-02-24 09:59:11 -0800

    practice from work 02/24/2016

--- ArrayLists/src/Contact.java
+++ ArrayLists/src/Contact.java
@@ -1,17 +1,21 @@
 public class Contact {
-   private String name;
-   private String phone;
+    private String name;
+    private String phone;
 
-   public Contact(String name, String phone) {
-      this.name = name;
-      this.phone = phone;
-   }
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
 
-   public String getName() {
-      return name;
-   }
+    public String getName() {
+        return name;
+    }
 
-   public String getPhone() {
-      return phone;
-   }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void printContact() {
+
+    }
 }
