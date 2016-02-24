dd25290ba3b37bb4ab0909bdb22c3a1925c93d14
alohageck0 <royalfiish@gmail.com>
2016-02-24 00:29:44 -0800

    home udemy practice 02/24/16

---
+++ ArrayLists/src/Challenge.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Challenge {
+   private static MobilePhone iphone = new MobilePhone();
+   private static Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) {
+      boolean quit = false;
+      int choice = 0;
+      printInstructions();
+      while (!quit) {
+         System.out.println("Enter you choice: ");
+         choice = scanner.nextInt();
+         scanner.nextLine();
+
+         switch (choice) {
+            case 0:
+               printInstructions();
+               break;
+            case 1:
+               quit = true;
+               break;
+            case 2:
+               iphone.printPhoneBook();
+               break;
+            case 3:
+               iphone.addContact();
+               break;
+            case 4:
+               iphone.updateName();
+               break;
+            case 5:
+               iphone.updatePhone();
+               break;
+            case 6:
+               iphone.deleteContact();
+            case 7:
+               iphone.findContact();
+         }
+      }
+   }
+
+   public static void printInstructions() {
+      System.out.println("0 - print instructions");
+      System.out.println("1 - Quit");
+      System.out.println("2 - print contacts");
+      System.out.println("3 - add new");
+      System.out.println("4 - update name");
+      System.out.println("5 - update phone");
+      System.out.println("6 - remove");
+      System.out.println("7 - find contact by name");
+   }
+}
---
+++ ArrayLists/src/MobilePhone.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MobilePhone {
+   private ArrayList<Contact> phoneBook = new ArrayList<>();
+   private static Scanner scanner = new Scanner(System.in);
+
+   public void addContact() {
+      System.out.println("Enter name: \r");
+      String name = scanner.nextLine();
+      System.out.println("Enter phone: \r");
+      String phone = scanner.nextLine();
+      if (ifExists(phoneBook, name) < 0) {
+         phoneBook.add(new Contact(name, phone));
+      }
+
+   }
+
+   public void deleteContact() {
+      System.out.println("Enter name: \r");
+      String name = scanner.nextLine();
+      if (ifExists(phoneBook, name) >= 0) {
+         phoneBook.remove(ifExists(phoneBook, name));
+      } else {
+         System.out.println("There is no such a contact");
+      }
+   }
+
+   public static int ifExists(ArrayList<Contact> arrayList, String name) {
+      for (Contact contact : arrayList) {
+         if (contact.getName() == name) {
+            return arrayList.indexOf(contact);
+         }
+      }
+      return -1;
+   }
+
+   public void printPhoneBook() {
+      for (int i = 0; i < phoneBook.size(); i++) {
+         System.out.println("Name " + phoneBook.get(i).getName() + ": " + phoneBook.get(i).getPhone());
+      }
+   }
+
+   public void updateName() {
+      System.out.println("Enter current name: \r");
+      String oldName = scanner.nextLine();
+      if (ifExists(phoneBook, oldName) >= 0) {
+         System.out.println("Enter new name: \r");
+         String newName = scanner.nextLine();
+         int index = ifExists(phoneBook, oldName);
+         phoneBook.set(index, new Contact(newName, phoneBook.get(index).getPhone()));
+      } else {
+         System.out.println("There is no such a contact");
+      }
+   }
+
+   public void updatePhone() {
+      System.out.println("Enter current name: \r");
+      String name = scanner.nextLine();
+      if (ifExists(phoneBook, name) >= 0) {
+         System.out.println("Enter new phone: \r");
+         String newPhone = scanner.nextLine();
+         int index = ifExists(phoneBook, name);
+         phoneBook.set(index, new Contact(name, newPhone));
+      } else {
+         System.out.println("There is no such a contact");
+      }
+   }
+
+   public void findContact() {
+      System.out.println("Enter name to find: \r");
+      String name = scanner.nextLine();
+      if (ifExists(phoneBook, name) >= 0) {
+         int i = ifExists(phoneBook, name);
+         System.out.println("Name " + phoneBook.get(i).getName() + ": " + phoneBook.get(i).getName());
+      } else {
+         System.out.println("Does not exist");
+      }
+   }
+
+}
