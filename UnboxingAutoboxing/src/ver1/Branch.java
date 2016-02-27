2df2061d3c19702731aab54217f418c812fa8e98
alohageck0 <royalfiish@gmail.com>
2016-02-26 19:37:48 -0800

    home udemy practice 02/26/16

---
+++ UnboxingAutoboxing/src/ver1/Branch.java
@@ -0,0 +1,46 @@
+package ver1;
+
+import java.util.ArrayList;
+
+public class Branch {
+   private String name;
+   private ArrayList<Customer> customers;
+
+   public Branch(String name) {
+      this.name = name;
+      this.customers = new ArrayList<>();
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public boolean newCustomer(String customerName, double initial) {
+      if (findCustomer(customerName) == null) {
+         this.customers.add(new Customer(customerName, initial));
+         return true;
+      }
+
+      System.out.println("");
+      return false;
+   }
+
+   public boolean addCustomerTransaction(String customerName, double amount) {
+      Customer existingCustomer = findCustomer(customerName);
+      if (existingCustomer != null) {
+         existingCustomer.addTransaction(amount);
+         return true;
+      }
+      return false;
+   }
+
+   private Customer findCustomer(String customerName) {
+      for (int i = 0; i < customers.size(); i++) {
+         Customer checkedCustomer = this.customers.get(i);
+         if (checkedCustomer.getName().equals(customerName)) {
+            return checkedCustomer;
+         }
+      }
+      return null;
+   }
+}
