cb19ec93191e296d34f50690e04b761300ddff88
alohageck0 <royalfiish@gmail.com>
2016-02-27 20:39:33 -0800

    home udemy practice 02/27/16

--- LinkedLists/src/Album.java
+++ LinkedLists/src/Album.java
@@ -1,2 +1,49 @@
+import java.util.ArrayList;
+
 public class Album {
+   private String albumName;
+   private ArrayList<Song> songList;
+
+   public Album(String albumName) {
+      this.albumName = albumName;
+      this.songList = new ArrayList<>();
+   }
+
+   public String getAlbumName() {
+      return albumName;
+   }
+
+   public ArrayList<Song> getSongList() {
+      return songList;
+   }
+
+   public boolean addSong(String songTitle, String songDuration) {
+      if (!findSong(songTitle)) {
+         this.songList.add(new Song(songTitle, songDuration));
+         System.out.println(songTitle + " was added to album ->" + this.albumName);
+         return true;
+      }
+      System.out.println("Song " + songTitle + " already in the playlist");
+      return false;
+   }
+
+   private boolean findSong(String songTitle) {
+      for (int i = 0; i < songList.size(); i++) {
+         if (songList.get(i).getSongTitle().equals(songTitle)) {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   public Song querySong(String songTitle) {
+      for (int i = 0; i < songList.size(); i++) {
+         Song song = songList.get(i);
+         if (song.getSongTitle().equals(songTitle)) {
+            return song;
+         }
+      }
+      return null;
+   }
 }
+
--- LinkedLists/src/Playlist.java
+++ LinkedLists/src/Playlist.java
@@ -1,2 +1,28 @@
+import java.util.LinkedList;
+
 public class Playlist {
+   private String playlistName;
+   private LinkedList<Song> songsPlaylist;
+
+   public Playlist(String playlistName) {
+      this.playlistName = playlistName;
+      this.songsPlaylist = new LinkedList<>();
+   }
+
+   public LinkedList<Song> getSongsPlaylist() {
+      return songsPlaylist;
+   }
+
+   public String getPlaylistName() {
+      return playlistName;
+   }
+
+   public boolean addSongFromAlbum(Album album, String songTitle) {
+      Song song = album.querySong(songTitle);
+      if (song != null) {
+         songsPlaylist.add(song);
+         return true;
+      }
+      return false;
+   }
 }
