e9a248266aa26db675c08795cb99a9581874b000
alohageck0 <royalfiish@gmail.com>
2016-02-27 18:48:05 -0800

    home udemy practice 02/27/16

--- LinkedLists/src/Main.java
+++ LinkedLists/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        //tes
+   public static void main(String[] args) {
+      //tes
 //todat
-    }
+      Customer customer = new Customer("Tim", 442.23);
+
+      Customer anotherCust;
+      anotherCust = customer;
+      anotherCust.setBalance(12.43);
+      System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+   }
 }
