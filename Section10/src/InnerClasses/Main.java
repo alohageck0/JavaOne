a80ea58ea009ba1c1574ad1f271434e334263472
alohageck0 <royalfiish@gmail.com>
2016-03-01 07:32:08 -0800

    home udemy practice 03/01/16

--- Section10/src/InnerClasses/Main.java
+++ Section10/src/InnerClasses/Main.java
@@ -2,6 +2,12 @@ package InnerClasses;
 
 public class Main {
    public static void main(String[] args) {
+      GearBox mcLaren = new GearBox(6);
+      GearBox.Gear first = mcLaren.new Gear(1, 12.3);
+//      GearBox.Gear second = new GearBox.Gear(2,12);
+//      GearBox.Gear thied = new mcLaren.Gear(2,12);
+
+      System.out.println(first.driveSpeed(1000));
 
    }
 }
