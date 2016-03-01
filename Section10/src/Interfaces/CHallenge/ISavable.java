b09ec60cc62ab5e29f76f7733324bb629f1e5708
alohageck0 <royalfiish@gmail.com>
2016-02-29 19:59:20 -0800

    home udemy practice 02/29/16

---
+++ Section10/src/Interfaces/CHallenge/ISavable.java
@@ -0,0 +1,10 @@
+package Interfaces.CHallenge;
+
+import java.util.ArrayList;
+
+public interface ISavable {
+   ArrayList<String> returnList();
+
+   void populateFields(String values);
+
+}
---
+++ Section10/src/Interfaces/CHallenge/Player.java
@@ -0,0 +1,15 @@
+package Interfaces.CHallenge;
+
+import java.util.ArrayList;
+
+public class Player implements ISavable {
+   @Override
+   public ArrayList<String> returnList() {
+      return null;
+   }
+
+   @Override
+   public void populateFields(String values) {
+
+   }
+}
