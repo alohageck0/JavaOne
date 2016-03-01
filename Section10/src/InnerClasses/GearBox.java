003158489234424b1f42912ca5d00cbe51b0b55d
alohageck0 <royalfiish@gmail.com>
2016-03-01 07:49:55 -0800

    home udemy practice 03/01/16

--- Section10/src/InnerClasses/GearBox.java
+++ Section10/src/InnerClasses/GearBox.java
@@ -13,6 +13,9 @@ public class GearBox {
       this.maxGears = maxGears;
       Gear neutral = new Gear(0, 0.0);
       this.gears.add(neutral);
+      for (int i = 0; i < maxGears; i++) {
+         addGear(i, i * 5.3);
+      }
    }
 
    public void operateClutch(boolean in) {
@@ -41,7 +44,7 @@ public class GearBox {
          System.out.println("Scream");
          return 0.0;
       }
-      return revs*gears.get(currentGear).getRatio();
+      return revs * gears.get(currentGear).getRatio();
    }
 
    private class Gear {
