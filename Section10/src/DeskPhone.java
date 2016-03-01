57332f04edbbed7d3d0f4950eff384ffa620f91f
alohageck0 <royalfiish@gmail.com>
2016-02-29 19:22:51 -0800

    home udemy practice 02/29/16

---
+++ Section10/src/DeskPhone.java
@@ -0,0 +1,41 @@
+public class DeskPhone implements ITelephone {
+   private int myNumber;
+   private boolean isRinging;
+
+   public DeskPhone(int m) {
+      this.myNumber = m;
+   }
+
+   @Override
+   public void powerOn() {
+      System.out.println("No action taken, deskphone does not have power button");
+   }
+
+   @Override
+   public void dial(int phoneNumber) {
+      System.out.println("Now ringing " + phoneNumber + " on deskphone");
+
+   }
+
+   @Override
+   public void answer() {
+      System.out.println("Answering deskphone");
+      isRinging = false;
+   }
+
+   @Override
+   public boolean callPhone(int phoneNumber) {
+      if (phoneNumber == myNumber) {
+         isRinging = true;
+         System.out.println("Ring Ring");
+      } else {
+         isRinging = false;
+      }
+      return isRinging;
+   }
+
+   @Override
+   public boolean isRinging() {
+      return isRinging;
+   }
+}
--- Section10/src/Main.java
+++ Section10/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+      ITelephone timsPhone;
+      timsPhone = new DeskPhone(123123123);
 
-    }
+      timsPhone.powerOn();
+      timsPhone.callPhone(123123123);
+      timsPhone.answer();
+   }
 }
