467d3732c844987f521a17a4f4a185d52982b00d
alohageck0 <royalfiish@gmail.com>
2016-03-02 20:39:01 -0800

    home udemy practice 03/02/16

---
+++ Section10/src/AbstractClasses/Challenge/TimsVersion/NodeList.java
@@ -0,0 +1,11 @@
+package AbstractClasses.Challenge.TimsVersion;
+
+public interface NodeList {
+   ListItem getRoot();
+
+   boolean addItem(ListItem listItem);
+
+   boolean removeItem(ListItem listItem);
+
+   void traverse(ListItem root);
+}
