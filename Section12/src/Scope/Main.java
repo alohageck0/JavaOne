3e37d656c7ebeab05ed0ca4d32ae6580910e6ddd
alohageck0 <royalfiish@gmail.com>
2016-03-05 12:26:20 -0800

    home udemy practice 03/05/16

--- Section12/src/Scope/Main.java
+++ Section12/src/Scope/Main.java
@@ -2,9 +2,14 @@ package Scope;
 
 public class Main {
    public static void main(String[] args) {
-String privateVar = "this is private to main()";
+      String privateVar = "this is private to main()";
       ScopeCheck scopeInstance = new ScopeCheck();
-      System.out.println("Scope instance privateVar is "+ scopeInstance.getPrivateVar());
+      System.out.println("Scope instance privateVar is " + scopeInstance.getPrivateVar());
       System.out.println(privateVar);
+
+      scopeInstance.timesTwo();
+      System.out.println("******");
+      ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+      innerClass.timesTwo();
    }
 }
--- Section12/src/Scope/ScopeCheck.java
+++ Section12/src/Scope/ScopeCheck.java
@@ -9,9 +9,31 @@ public class ScopeCheck {
    }
 
 
-
    public int getPrivateVar() {
       return privateVar;
+   }
+
+   public void timesTwo() {
+      int privateVar = 2;
+      for (int i = 0; i < 10; i++) {
+         System.out.println(i + " times two = " + i * privateVar);
+      }
+   }
+
+   public class InnerClass {
+      public int privateVar = 3;
+
+      public InnerClass() {
+         System.out.println("Inner class created, privateVar is " + privateVar);
+      }
+
+      public void timesTwo() {
+//         int privateVar = 2;
+         ScopeCheck.this.timesTwo();
+         for (int i = 0; i < 10; i++) {
+            System.out.println(i + " times two = " + i * ScopeCheck.this.privateVar);
+         }
+      }
    }
 }
 
