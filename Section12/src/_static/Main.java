100309f9a62fd3891cdbb3b986432f433457f114
alohageck0 <royalfiish@gmail.com>
2016-03-05 19:53:16 -0800

    home udemy practice 03/05/16

--- Section12/src/_static/Main.java
+++ Section12/src/_static/Main.java
@@ -1,13 +1,22 @@
 package _static;
 
 public class Main {
-   public static void main(String[] args) {
-      StaticTest firstInstance = new StaticTest("1st inst");
-      System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
-      StaticTest secondInstance = new StaticTest("2nd");
-      System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
-      StaticTest thInstance = new StaticTest("3rd");
-      System.out.println(thInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+   public static int multiplier = 7;
 
+   public static void main(String[] args) {
+//      StaticTest firstInstance = new StaticTest("1st inst");
+//      System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//      StaticTest secondInstance = new StaticTest("2nd");
+//      System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//      StaticTest thInstance = new StaticTest("3rd");
+//      System.out.println(thInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+      int answer = multiply(6);
+      System.out.println(answer);
+      System.out.println("Multiplier is " + multiplier);
+   }
+
+   public static int multiply(int number) {
+      return number * multiplier;
    }
 }
