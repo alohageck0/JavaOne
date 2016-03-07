a6fa6a6f31eb218cac205bbb9c185f6d176b014d
alohageck0 <royalfiish@gmail.com>
2016-03-06 18:32:41 -0800

    home udemy practice 03/06/16

--- Section13/src/CollectionPractice/Main.java
+++ Section13/src/CollectionPractice/Main.java
@@ -4,7 +4,17 @@ public class Main {
    public static void main(String[] args) {
       //test
 
-
-
+      Theatre theatre = new Theatre("Olympium", 8, 12);
+//      theatre.getSeats();
+      if (theatre.reserveSeat("H11")) {
+         System.out.println("please pay");
+      } else {
+         System.out.println("Sorry already taken");
+      }
+      if (theatre.reserveSeat("H11")) {
+         System.out.println("please pay");
+      } else {
+         System.out.println("Sorry already taken");
+      }
    }
 }
