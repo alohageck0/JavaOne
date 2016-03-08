e60d4f89bfe9de2bfa4ac8380d452d9b295be304
alohageck0 <royalfiish@gmail.com>
2016-03-08 07:36:22 -0800

    home udemy practice 03/08/16

--- Section13/src/CollectionPractice/Adventure/Location.java
+++ Section13/src/CollectionPractice/Adventure/Location.java
@@ -12,6 +12,7 @@ public class Location {
       this.locationID = locationID;
       this.description = description;
       exits = new HashMap<>();
+      exits.put("Q", 0);
    }
 
    public void addExit(String direction, int location) {
@@ -27,7 +28,7 @@ public class Location {
    }
 
    public Map<String, Integer> getExits() {
-      return new HashMap<String, Integer>(exits);
+      return new HashMap<>(exits);
    }
 
 }
