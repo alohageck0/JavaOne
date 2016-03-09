3010df34f2637b43ae770637ab41d94357ac4e2e
alohageck0 <royalfiish@gmail.com>
2016-03-08 22:24:21 -0800

    home udemy practice 03/08/16

--- Section13/src/CollectionPractice/Adventure/Main.java
+++ Section13/src/CollectionPractice/Adventure/Main.java
@@ -10,28 +10,34 @@ public class Main {
 
    public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
+      Map<String, Integer> tempExit = new HashMap<>();
 
-      locations.put(0, new Location(0, "location0"));
-      locations.put(1, new Location(1, "location1"));
-      locations.put(2, new Location(2, "location2"));
-      locations.put(3, new Location(3, "location3"));
-      locations.put(4, new Location(4, "location4"));
-      locations.put(5, new Location(5, "location5"));
+      locations.put(0, new Location(0, "location0", tempExit));
 
-      locations.get(1).addExit("W", 2);
-      locations.get(1).addExit("E", 3);
-      locations.get(1).addExit("S", 4);
-      locations.get(1).addExit("N", 5);
+      tempExit = new HashMap<>();
+      tempExit.put("W", 2);
+      tempExit.put("E", 3);
+      tempExit.put("S", 4);
+      tempExit.put("N", 5);
+      locations.put(1, new Location(1, "location1", tempExit));
 
-      locations.get(2).addExit("N", 5);
+      tempExit = new HashMap<>();
+      tempExit.put("N", 5);
+      locations.put(2, new Location(2, "location2", tempExit));
 
-      locations.get(3).addExit("W", 1);
+      tempExit = new HashMap<>();
+      tempExit.put("W", 1);
+      locations.put(3, new Location(3, "location3", tempExit));
 
-      locations.get(4).addExit("N", 1);
-      locations.get(4).addExit("W", 2);
+      tempExit = new HashMap<>();
+      tempExit.put("N", 1);
+      tempExit.put("W", 2);
+      locations.put(4, new Location(4, "location4", tempExit));
 
-      locations.get(5).addExit("S", 1);
-      locations.get(5).addExit("W", 2);
+      tempExit = new HashMap<>();
+      tempExit.put("S", 1);
+      tempExit.put("W", 2);
+      locations.put(5, new Location(5, "location5", tempExit));
 
       directions.put("SOUTH", "S");
       directions.put("NORTH", "N");
@@ -43,6 +49,7 @@ public class Main {
       int loc = 1;
       while (true) {
          System.out.println(locations.get(loc).getDescription());
+         tempExit.remove("S");
          if (loc == 0) {
             break;
          }
