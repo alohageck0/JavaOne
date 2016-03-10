c64c299a70ea63aff406ae479a1cc5b043c0e83a
alohageck0 <royalfiish@gmail.com>
2016-03-09 23:01:06 -0800

    home udemy practice 03/09/16

--- Section13/src/HashSetsPractice/Challenge/HeavenlyBody.java
+++ Section13/src/HashSetsPractice/Challenge/HeavenlyBody.java
@@ -3,13 +3,13 @@ package HashSetsPractice.Challenge;
 public class HeavenlyBody {
    private final String name;
    private final double orbitalPeriod;
-   private final String bodyTupe;
+   private final String bodyType;
 
-   public HeavenlyBody(String name, double orbitalPeriod, String bodyTupe) {
+   public HeavenlyBody(String name, double orbitalPeriod, String bodyType) {
       this.name = name;
       this.orbitalPeriod = orbitalPeriod;
 //      this.satellites = new HashSet<>();
-      this.bodyTupe = bodyTupe;
+      this.bodyType = bodyType;
    }
 
    public String getName() {
@@ -20,7 +20,9 @@ public class HeavenlyBody {
       return orbitalPeriod;
    }
 
-
+   public String getBodyType() {
+      return bodyType;
+   }
 
    @Override
    public boolean equals(Object obj) {
@@ -29,12 +31,19 @@ public class HeavenlyBody {
       }
       System.out.println("obj.getClass() is " + obj.getClass());
       System.out.println("this.getClass() is " + this.getClass());
-      if ((obj == null) || (obj.getClass() != this.getClass())) {
+      if ((obj == null)) {
          return false;
       }
       String objName = ((HeavenlyBody) obj).getName();
-      return this.name.equals(objName);
-
+      String objBodyType = ((HeavenlyBody) obj).getBodyType();
+      if (this.name.equals(objName)) {
+         if (this.bodyType.equals(objBodyType)) {
+            return false;
+         } else {
+            return true;
+         }
+      }
+      return false;
    }
 
    @Override
--- Section13/src/HashSetsPractice/Challenge/SetChallenge.java
+++ Section13/src/HashSetsPractice/Challenge/SetChallenge.java
@@ -1,7 +1,45 @@
 package HashSetsPractice.Challenge;
 
-public class SetChallenge {
-   public static void main(String[] args) {
+import java.util.HashSet;
+import java.util.Set;
 
+public class SetChallenge {
+   private static Set<HeavenlyBody> solarSystem = new HashSet<>();
+
+   public static void main(String[] args) {
+      HeavenlyBody temp = new Planet("Mercury", 88);
+      solarSystem.add(temp);
+
+      temp = new Planet("Venus", 225);
+      solarSystem.add(temp);
+
+      temp = new Planet("Pluto", 248);
+      solarSystem.add(temp);
+
+      temp = new Planet("Earth", 265);
+      solarSystem.add(temp);
+
+      HeavenlyBody tempMoon = new Moon("Moon", 27);
+      solarSystem.add(tempMoon);
+
+      getPlanet("Earth").addMoon(getMoon("Moon"));
+   }
+
+   public static Planet getPlanet(String name) {
+      for (HeavenlyBody h : solarSystem) {
+         if (h.getName().equals(name) && h.getBodyType().equals("Planet")) {
+            return (Planet) h;
+         }
+      }
+      return null;
+   }
+
+   public static Moon getMoon(String name) {
+      for (HeavenlyBody h : solarSystem) {
+         if (h.getName().equals(name) && h.getBodyType().equals("Moon")) {
+            return (Moon) h;
+         }
+      }
+      return null;
    }
 }
