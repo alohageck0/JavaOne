8e5a679baadd68a34c03f363ac86818f75dbf278
Evgenii <royalfiish@gmail.com>
2016-03-10 09:17:18 -0800

    practice from work 03/10/2016

--- Section12/src/Main.java
+++ Section12/src/Main.java
@@ -1,15 +1,15 @@
-import com.example.mypackage.Series;
-
-public class Main {
-   public static void main(String[] args) {
-      for (int i = 0; i <= 10; i++) {
-         System.out.println(Series.nSum(i));
-      }
-      for (int i = 0; i <= 10; i++) {
-         System.out.println(Series.factorial(i));
-      }
-      for (int i = 0; i <= 10; i++) {
-         System.out.println(Series.fibonacci(i));
-      }
-   }
-}
+//import com.example.mypackage.Series;
+//
+//public class Main {
+//   public static void main(String[] args) {
+//      for (int i = 0; i <= 10; i++) {
+//         System.out.println(Series.nSum(i));
+//      }
+//      for (int i = 0; i <= 10; i++) {
+//         System.out.println(Series.factorial(i));
+//      }
+//      for (int i = 0; i <= 10; i++) {
+//         System.out.println(Series.fibonacci(i));
+//      }
+//   }
+//}
