6b967a17cbb89a17f4cd1d836c077499015c2729
alohageck0 <royalfiish@gmail.com>
2016-03-13 20:14:01 -0700

    home udemy practice 03/13/16

--- Section13/src/HashSetsPractice/Challenge/Tims/Main.java
+++ Section13/src/HashSetsPractice/Challenge/Tims/Main.java
@@ -105,8 +105,13 @@ public class Main {
 //         System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
       }
 
-      Object o = new Object();
-      o.equals(o);
-      "pluto".equals("");
+      HeavenlyBody earth1 = new Planet("Earth",365);
+      HeavenlyBody earth2 = new Planet("Earth",365);
+      System.out.println(earth1.equals(earth2));
+      System.out.println(earth2.equals(earth1));
+
+      System.out.println(earth1.equals(pluto));
+      System.out.println(pluto.equals(earth1));
    }
+
 }
