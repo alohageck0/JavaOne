2759d6459e2c4b514b49f66f60738091b63fb674
alohageck0 <royalfiish@gmail.com>
2016-03-14 23:07:54 -0700

    home udemy practice 03/14/16

--- Section13/src/SortedCollections/StockList.java
+++ Section13/src/SortedCollections/StockList.java
@@ -1,5 +1,6 @@
 package SortedCollections;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,21 +17,45 @@ public class StockList {
          StockItem inStock = list.getOrDefault(item.getName(), item);
          //if there are already stocks on this item, adjust quantity
          if (inStock != item) {
-            item.adjustStock(inStock.getQuantityStock());
+            item.adjustStock(inStock.quantityInStock());
          }
          list.put(item.getName(), item);
-         return item.getQuantityStock();
+         return item.quantityInStock();
       }
       return 0;
    }
 
    public int sellStock(String item, int quantity) {
       StockItem inStock = list.getOrDefault(item, null);
-      if ((inStock != null) && (inStock.getQuantityStock() >= quantity) && (quantity > 0)) {
+      if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)) {
          inStock.adjustStock(-quantity);
          return quantity;
       }
       return 0;
+   }
+
+   public StockItem get(String itemName) {
+      return this.get(itemName);
+   }
+
+   public Map<String, StockItem> Items() {
+      return Collections.unmodifiableMap(list);
+   }
+
+   @Override
+   public String toString() {
+      String s = "\nStock List\n";
+      double totalCost = 0.0;
+      for (Map.Entry<String, StockItem> itemEntry : list.entrySet()) {
+         StockItem stockItem = itemEntry.getValue();
+
+         double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+         s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
+         s = s + itemValue + "\n";
+         totalCost += itemValue;
+
+      }
+      return s + "Total stock value " + totalCost;
    }
 
 }
