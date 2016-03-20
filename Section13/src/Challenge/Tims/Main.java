21c0974cbcf29e967192780808f41c2656a5e852
alohageck0 <royalfiish@gmail.com>
2016-03-20 14:00:25 -0700

    home udemy practice 03/19/16

---
+++ Section13/src/Challenge/Tims/Main.java
@@ -0,0 +1,80 @@
+package Challenge.Tims;
+
+import java.util.Map;
+
+public class Main {
+   private static StockList stockList = new StockList();
+
+   public static void main(String[] args) {
+      StockItem temp = new StockItem("bread", 0.86, 100);
+      stockList.addStock(temp);
+
+      temp = new StockItem("Cake", 1.1, 7);
+      stockList.addStock(temp);
+
+      temp = new StockItem("car", 12.5, 2);
+      stockList.addStock(temp);
+      temp = new StockItem("chair", 62.0, 10);
+      stockList.addStock(temp);
+      temp = new StockItem("cup", 0.5, 200);
+      stockList.addStock(temp);
+
+      temp = new StockItem("cup", 0.45, 7);
+      stockList.addStock(temp);
+
+
+      temp = new StockItem("door", 72.95, 4);
+      stockList.addStock(temp);
+
+      temp = new StockItem("juice", 2.5, 25);
+      stockList.addStock(temp);
+
+      System.out.println(stockList);
+      for (String s : stockList.Items().keySet()) {
+         System.out.println(s);
+      }
+
+      Basket basket = new Basket("Evgenii");
+      sellItem(basket, "car", 1);
+      System.out.println(basket);
+
+      sellItem(basket, "car", 1);
+      System.out.println(basket);
+
+      sellItem(basket, "car", 1);
+      sellItem(basket, "spanner", 5);
+      System.out.println(basket);
+
+      sellItem(basket, "juice", 4);
+      sellItem(basket, "cup", 12);
+      sellItem(basket, "bread", 1);
+      System.out.println(basket);
+      System.out.println(stockList);
+
+//      temp = new StockItem("pen",1.12);
+//      stockList.Items().put(temp.getName(),temp);
+
+      stockList.Items().get("car").adjustStock(2000);
+      stockList.get("car").adjustStock(-1000);
+      System.out.println(stockList);
+      for (Map.Entry<String, Double> price : stockList.PriceList().entrySet()) {
+         System.out.println(price.getKey() + " costs " + price.getValue());
+      }
+
+
+   }
+
+   public static int sellItem(Basket basket, String item, int quantity) {
+      //retreive item from stock
+      StockItem stockItem = stockList.get(item);
+      if (stockItem == null) {
+         System.out.println("We do not's sell " + item);
+         return 0;
+      }
+      if (stockList.sellStock(item, quantity) != 0) {
+         basket.addToBasket(stockItem, quantity);
+         return quantity;
+      }
+      return 0;
+   }
+}
---
+++ Section13/src/Challenge/Tims/MainChallenge.java
@@ -0,0 +1,7 @@
+package Challenge.Tims;
+
+public class MainChallenge {
+   public static void main(String[] args) {
+      //ss
+   }
+}
