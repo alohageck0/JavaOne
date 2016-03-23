2c099952e076fe5dafb08699905f64097111d331
alohageck0 <royalfiish@gmail.com>
2016-03-22 20:18:36 -0700

    home udemy practice 03/21/16

--- Section14/src/JavaFXPractice/Intro.java
+++ Section14/src/JavaFXPractice/Intro.java
@@ -3,6 +3,29 @@ package JavaFXPractice;
 public class Intro {
    public static void main(String[] args) {
       //start
-      //rr
+      StairCase(6);
+   }
+
+   static void StairCase(int n) {
+      String[] arr = new String[n];
+      for (int i = 0; i < n; i++) {
+         arr[i] = " ";
+
+      }
+      for (int i = n - 1; i >= 0; i--) {
+         arr[i] = "#";
+         for (int j = 0; j < n; j++) {
+            System.out.print(arr[j]);
+         }
+         System.out.println();
+      }
+   }
+
+   static long sumOfIntegers(int[] arr) {
+      long sum = 0;
+      for (int i = 0; i < arr.length; i++) {
+         sum += arr[i];
+      }
+      return sum;
    }
 }
