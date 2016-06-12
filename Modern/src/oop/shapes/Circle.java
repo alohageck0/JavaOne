c324a8842e1e480239c6040e31299beceb15c987
alohageck0 <royalfiish@gmail.com>
2016-06-12 11:07:37 -0700

    06/12/2016

--- Modern/src/oop/shapes/Circle.java
+++ Modern/src/oop/shapes/Circle.java
@@ -1,5 +1,13 @@
 package oop.shapes;
 
 public class Circle {
+   private double radius;
 
+   public Circle(double radius) {
+      this.radius = radius;
+   }
+
+   public double computeArea() {
+      return Math.PI * Math.pow(radius, 2);
+   }
 }
--- Modern/src/oop/shapes/CircleTest.java
+++ Modern/src/oop/shapes/CircleTest.java
@@ -1,4 +1,8 @@
 package oop.shapes;
 
 public class CircleTest {
+   public static void main(String[] args) {
+      Circle circle = new Circle(4);
+      System.out.println(circle.computeArea());
+   }
 }
