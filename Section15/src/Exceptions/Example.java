1c38839405769ebbb92ba8e41de866fe7604fcab
alohageck0 <royalfiish@gmail.com>
2016-06-27 22:03:30 -0700

    06/27/2016

---
+++ Section15/src/Exceptions/Example.java
@@ -0,0 +1,46 @@
+package Exceptions;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Example {
+   public static void main(String[] args) {
+      try {
+         int result = divide();
+         System.out.println(result);
+      } catch (ArithmeticException | NoSuchElementException e) {
+         System.out.println(e.toString());
+         System.out.println("Unable to perform division");
+      }
+   }
+
+   public static int divide() {
+      int x, y;
+//      try {
+      x = getInt();
+      y = getInt();
+      System.out.println("x is " + x + ", y is " + y);
+      return x / y;
+//      } catch (NoSuchElementException e) {
+//         throw new NoSuchElementException("No suitable input");
+//      } catch (ArithmeticException e) {
+//         throw new ArithmeticException("Attempt divide by zero");
+//      }
+
+   }
+
+   private static int getInt() {
+      Scanner s = new Scanner(System.in);
+      System.out.println("Enter an integer");
+      while (true) {
+         try {
+            return s.nextInt();
+         } catch (InputMismatchException e) {
+            // go round again
+            s.nextLine();
+            System.out.println("Please enter a number using obly a digits 0-9");
+         }
+      }
+   }
+}
