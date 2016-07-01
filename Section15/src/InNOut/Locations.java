d6bbf7f86971e83f385df3e2ddf270dbaefc706e
alohageck0 <royalfiish@gmail.com>
2016-06-30 21:38:02 -0700

    06/30/2016

--- Section15/src/InNOut/Locations.java
+++ Section15/src/InNOut/Locations.java
@@ -1,5 +1,6 @@
 package InNOut;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -56,6 +57,37 @@ public class Locations implements Map<Integer, Location> {
             scanner.close();
          }
       }
+      //read exitst
+      try {
+         scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+         scanner.useDelimiter(", ");
+         while (scanner.hasNextLine()) {
+//            int loc = scanner.nextInt();
+//            scanner.skip(scanner.delimiter());
+//            String direction = scanner.next();
+//            scanner.skip(scanner.delimiter());
+//            String dest = scanner.nextLine();
+//            int destination = Integer.parseInt(dest);
+            String input = scanner.nextLine();
+            String[] data = input.split(", ");
+
+            int loc = Integer.parseInt(data[0]);
+            String direction = data[1];
+            int destination = Integer.parseInt(data[2]);
+            System.out.println(loc + ": " + direction + ": " + destination);
+            Location location = locations.get(loc);
+            location.addExit(direction, destination);
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+      } finally {
+         if (scanner != null) {
+            scanner.close();
+
+         }
+      }
+
+
 //      Map<String, Integer> tempExit = new HashMap<String, Integer>();
 //      locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
 //
