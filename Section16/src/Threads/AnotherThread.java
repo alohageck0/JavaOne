6b59003b0eee0c862a134422352dac2fc71095f7
alohageck0 <royalfiish@gmail.com>
2016-07-07 21:17:11 -0700

    07/07/2016

--- Section16/src/Threads/AnotherThread.java
+++ Section16/src/Threads/AnotherThread.java
@@ -1,10 +1,18 @@
 package Threads;
 
 import static Threads.ThreadColor.ANSI_BLUE;
+import static Threads.ThreadColor.ANSI_RED;
 
 public class AnotherThread extends Thread {
    @Override
    public void run() {
-      System.out.println(ANSI_BLUE + "Hello from another thread");
+      System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+      try {
+         Thread.sleep(3000);
+      } catch (InterruptedException e) {
+         System.out.println(ANSI_RED + "Another thread woke me up");
+      }
+
+      System.out.println(ANSI_BLUE + "3 secs passes and I'm awake");
    }
 }
--- Section16/src/Threads/Main.java
+++ Section16/src/Threads/Main.java
@@ -1,5 +1,6 @@
 package Threads;
 
+import static Threads.ThreadColor.ANSI_GREEN;
 import static Threads.ThreadColor.ANSI_PURPLE;
 import static Threads.ThreadColor.ANSI_RED;
 
@@ -7,14 +8,25 @@ public class Main {
    public static void main(String[] args) {
       System.out.println(ANSI_PURPLE + "Hello from the main thread");
       Thread anotherThread = new AnotherThread();
+      anotherThread.setName("===Another thread====");
       anotherThread.start();
 
       new Thread() {
          public void run() {
-            System.out.println(ANSI_RED + "Hello from anon class thread");
+            System.out.println(ANSI_GREEN + "Hello from anon class thread");
+         }
+      }.start();
+//
+//      Thread myRunnable = new Thread(new MyRunnable());
+//      myRunnable.start();
+
+      new Thread(new MyRunnable()) {
+         @Override
+         public void run() {
+            System.out.println(ANSI_RED + "Hello from anon implementation ");
          }
       }.start();
 
-      System.out.println(ANSI_PURPLE+ "Again from maIn thread");
+      System.out.println(ANSI_PURPLE + "Again from maIn thread");
    }
 }
