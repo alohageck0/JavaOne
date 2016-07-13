91fb529a8c92a42d12a27c9d15e0528b7a9b51d3
alohageck0 <royalfiish@gmail.com>
2016-07-12 20:58:57 -0700

    07/12/2016

---
+++ Modern/src/lists/Main.java
@@ -0,0 +1,21 @@
+package lists;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+   public static void main(String[] args) {
+      List<String> names = new LinkedList<>();
+      names.add("Marty");
+      names.add("David");
+      names.add("Cay");
+      int numEntries = names.size();
+      System.out.println("number of entries " + numEntries);
+      String secondElem = names.get(1);
+      System.out.println("Second element " + secondElem);
+      boolean containsJoe = names.contains("Joe");
+      System.out.println("Contains Joe - " + containsJoe);
+      names.remove("Cay");
+      System.out.println(names);
+   }
+}
