a3394b9ffda77633c8d17450a330b6bcd8c6b421
Evgenii <royalfiish@gmail.com>
2016-08-19 10:31:17 -0700

    practice from work 03/22/2016

--- Modern/src/oop/shapes_abstract/Main.java
+++ Modern/src/oop/shapes_abstract/Main.java
@@ -1,10 +1,31 @@
 package oop.shapes_abstract;
 
+import org.testng.annotations.Test;
+import org.testng.mustache.StringChunk;
+
 public class Main {
+
+   @Test
+   public void test(){
+      reverseString("asdfg");
+      System.out.println();
+      System.out.println(reverseString1("asdfg"));
+   }
    public static void main(String[] args) {
       Shape[] shapes = {new Circle(5),
               new Rectangle(3, 5),
               new Square(5)};
       System.out.println(Shape.sumArea(shapes));
    }
+
+   public void reverseString(String input){
+      for (int i=input.length()-1;i>=0;i--) {
+         System.out.print(input.charAt(i));
+      }
+   }
+   public String reverseString1(String input){
+      StringBuilder stringBuilder = new StringBuilder(input);
+      return stringBuilder.reverse().toString();
+   }
+
 }
--- Modern/src/oop/shapes_interface/Main.java
+++ Modern/src/oop/shapes_interface/Main.java
@@ -1,11 +1,13 @@
 package oop.shapes_interface;
 
 
+import static java.lang.System.out;
+
 public class Main {
    public static void main(String[] args) {
       Shape[] shapes = {new Circle(5),
               new Rectangle(3, 5),
               new Square(5)};
-      System.out.println(Shape.sumArea(shapes));
+
    }
 }
